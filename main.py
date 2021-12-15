18d4a713e742c478f630c04e3c9b9ca65a97ec82
HUA343 <93173938+HUA343@users.noreply.github.com>
2021-12-15 10:35:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from selenium.webdriver.chrome import options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import Firefox, Chrome, PhantomJS
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
@@ -96,7 +98,7 @@ def submit(driver):
     print("Done")
     time.sleep(TIMESLP)
 
-# TODO
+
 def click_in_out(driver):
     try:
         driver.find_element_by_xpath('//*[@id="bizTip"]/div/div/div[1]/div/div/table/tbody/tr[11]/td/a').click()
@@ -104,38 +106,40 @@ def click_in_out(driver):
         time.sleep(TIMESLP)
     except:
         pass
-    driver.find_element_by_xpath('/html/body/div/section/div/div/div[2]/main/div[3]/a/div/div').click()
+    driver.find_element_by_link_text('出入校备案').click()
     print("进入出入校备案")
     time.sleep(TIMESLP)
-    driver.find_element_by_xpath('/html/body/div/section/div/div/div[2]/main/div/div[2]/form/div[3]/div[1]/div/div/div/button[1]').click()
+    temp = driver.find_elements_by_class_name("el-button")
+    temp[1].click()
     print("加载出校申请信息")
     time.sleep(TIMESLP)
-    driver.find_element_by_xpath('/html/body/div[1]/section/div/div/div[2]/main/div[1]/div[2]/form/div[3]/div[9]/div/div/label/span[1]/span').click()
+    driver.find_element_by_class_name("el-checkbox__inner").click()
     print("点击遵守")
     time.sleep(TIMESLP)
-    driver.find_element_by_xpath('/html/body/div[1]/section/div/div/div[2]/main/div[1]/div[2]/form/div[3]/div[10]/div/div/div/div[1]/button').click()
+    temp[3].click()
     print("保存")
     time.sleep(TIMESLP)
     ActionChains(driver).send_keys(Keys.ENTER).perform()
-    print("确认")
+    print("提交")
     time.sleep(TIMESLP)
-    driver.find_element_by_xpath('/html/body/div[1]/section/div/div/div[2]/main/div/div[1]/div/div/div[1]/div').click()
+    driver.find_element_by_class_name("el-page-header__left").click()
     print("返回上一页")
     time.sleep(TIMESLP)
-    driver.find_element_by_xpath('/html/body/div/section/div/div/div[2]/main/div[3]/a/div/div').click()
+    driver.find_element_by_link_text('出入校备案').click()
     print("进入出入校备案")
     time.sleep(TIMESLP)
-    driver.find_element_by_xpath('/html/body/div[1]/section/div/div/div[2]/main/div[1]/div[2]/form/div[3]/div[1]/div/div/div/button[2]').click()
+    temp = driver.find_elements_by_class_name("el-button")
+    temp[2].click()
     print("加载入校申请信息")
     time.sleep(TIMESLP)
-    driver.find_element_by_xpath('/html/body/div[1]/section/div/div/div[2]/main/div[1]/div[2]/form/div[3]/div[9]/div/div/label/span[1]/span').click()
+    driver.find_element_by_class_name("el-checkbox__inner").click()
     print("点击遵守")
     time.sleep(TIMESLP)
-    driver.find_element_by_xpath('/html/body/div[1]/section/div/div/div[2]/main/div[1]/div[2]/form/div[3]/div[10]/div/div/div/div[1]/button').click()
+    temp[3].click()
     print("保存")
     time.sleep(TIMESLP)
     ActionChains(driver).send_keys(Keys.ENTER).perform()
-    print("确认")
+    print("提交")
 
 def fill(driver):
     if CONFIGCODE == 0:
@@ -166,15 +170,24 @@ if __name__ == '__main__':
 
     # driver = Firefox()
     # driver = Chrome()
-
+    dir_path = os.path.dirname(os.path.abspath(__file__))
     if sys.platform == 'darwin':  # macOS
-        phantomjs_path = os.path.join('phantomjs', 'phantomjs-darwin')
+        # phantomjs_path = os.path.join('phantomjs', 'phantomjs-darwin')
+        chrome_path = os.path.join( dir_path, 'chromedriver_mac64', 'chromedriver')
     elif sys.platform == 'linux':  # linux
-        phantomjs_path = os.path.join('phantomjs', 'phantomjs-linux-x86_64')
+        # phantomjs_path = os.path.join('phantomjs', 'phantomjs-linux-x86_64')
+        chrome_path = os.path.join( dir_path, 'chromedriver_linux64', 'chromedriver')
     else:  # windows
-        phantomjs_path = os.path.join('phantomjs', 'phantomjs-windows.exe')
+        # phantomjs_path = os.path.join('phantomjs', 'phantomjs-windows.exe')
+        chrome_path = os.path.join( dir_path, 'chromedriver_win32', 'chromedriver.exe')
 
-    driver = PhantomJS(executable_path=phantomjs_path)
+    # driver = PhantomJS(executable_path=phantomjs_path)
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--headless')
+    print(chrome_path)
+    driver = webdriver.Chrome(executable_path = chrome_path, options = chrome_options)
 
     run(driver, args.username, args.password)
 
